d7647f79fbf3b2bcfbd01aeb61033147b07464e4
Aleksandar Nedelkovski <aleks@nedelkovskialeks.com>
2019-10-17 14:24:39 -0500

    level 2 - database encryption and enviromantal variables

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 //jshint esversion:6
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
+var encrypt = require('mongoose-encryption');
 
 const app = express();
 
@@ -12,24 +14,28 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true, useUnifiedTopology: true })
 
-const userSchema = {
+const userSchema = new mongoose.Schema({
     email: String,
     password: String
-}
+});
+
+const secret = process.env.SECRET;
+userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"] });
+
 
 const User = new mongoose.model("User", userSchema);
 
 app.get("/", function (req, res) {
     res.render("home");
-})
+});
 
 app.get("/login", function (req, res) {
     res.render("login");
-})
+});
 
 app.get("/register", function (req, res) {
     res.render("register");
-})
+});
 
 app.post("/register", function (req, res) {
     const newUser = new User({
@@ -50,18 +56,18 @@ app.post("/login", function (req, res) {
     const username = req.body.username;
     const password = req.body.password;
 
-    User.findOne({email: username}, function(err, foundUser){
-        if(err) {
+    User.findOne({ email: username }, function (err, foundUser) {
+        if (err) {
             console.log(err);
         } else {
-            if(foundUser) {
-                if(foundUser.password === password) {
+            if (foundUser) {
+                if (foundUser.password === password) {
                     res.render("secrets");
                 }
             }
         }
     })
-})
+});
 
 app.listen(8000, function () {
     console.log("Server started on port 3000");
